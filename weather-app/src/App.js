2ce38d2a99ce2e90e111a77d6db0c61996f08d4c
Anjali Akula <22.akulaa@gmail.com>
2021-03-16 15:39:03 -0400

    Added date and weather data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import render from 'react-dom';
 
 //shows daily weather
 function App() {
+  // source: https://reactgo.com/react-get-current-date/
+  const today = new Date()
+  const currDate = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +17,14 @@ function App() {
 
       <div class="left-side">
           <h3> Pittsburgh, PA </h3>
+          <h3> Date: { currDate } </h3>
       </div>
-      
+
+      <div class = "right-side">
+        <h3> Today is Sunny </h3>
+        <h3> Temperture: 82F </h3>
+      </div>
+
       <footer>
         <a href = "index.js"> Click here to view weekly weather data </a>
       </footer>
