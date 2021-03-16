91e8bceec155510f2ab918a8cac8e18cc0800ec0
Anjali Akula <22.akulaa@gmail.com>
2021-03-16 15:14:36 -0400

    Created React app

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome to the Weather App!</h1>
+        <a href = "index.js"> Link to Index Page </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
