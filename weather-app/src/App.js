213b5edb5e5f4a3e95c0eb146c9f5198962bbf26
Anjali Akula <22.akulaa@gmail.com>
2021-03-16 22:17:52 -0400

    Fixed navbar

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import background from "./home-background.png"
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
-import icons from 'App.html';
+import WbSunnyOutlinedIcon from '@material-ui/icons/WbSunnyOutlined';
 import { Button, Card, Row, Col } from 'react-materialize';
 // import render from 'react-dom';
 
@@ -17,57 +17,60 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h4> The Weather App!</h4>
+        <div class = "row">
+          <div class = "col s1"> <WbSunnyOutlinedIcon></WbSunnyOutlinedIcon> </div>
+          <div class = "col s4 offset-s3"> <h6> The Weather App</h6> </div>
+        </div>
       </header>
 
       <body class= "App-body">
         <div styles = {{ backgroundImage : `url(${background})` }}>
-        <div class = "row">
-          <div class="col s4">
-              <h3> Pittsburgh, PA </h3>
-              <h3> Date: { currDate } </h3>
-          </div>
+        <div class = "App-daily-view"> 
+          <div class = "row">
+            <div class="col s6">
+                <h4> Pittsburgh, PA </h4>
+                <h2> { currDate } </h2>
+            </div>
 
-          <div class="col s4">
-            <i class="material-icons"> wb_sunny </i>
-          </div>
-
-          <div class = "col s4">
-            <h3> Today is Sunny </h3>
-            <h3> Temperature: 56F </h3>
+            <div class = "col s6">
+              <h4> Today is Sunny </h4>
+              <h2> 56F </h2>
+            </div>
           </div>
         </div>
 
-        <h3> Weekly Weather Forecast </h3>
-        <table class = "App-table">
-          <tr>
-            <th> Monday </th>
-            <th> Tuesday </th>
-            <th> Wednesday </th>
-            <th> Thursday </th>
-            <th> Friday </th>
-            <th> Saturday </th>
-            <th> Sunday </th>
-          </tr>
-          <tr>
-            <td> 52F </td>
-            <td> 48F </td>
-            <td> 50F </td>
-            <td> 58F </td>
-            <td> 52F </td>
-            <td> 53F </td>
-            <td> 45F </td>
-          </tr>
-          <tr>
-            <td> 30F </td>
-            <td> 37F </td>
-            <td> 32F </td>
-            <td> 40F </td>
-            <td> 45F </td>
-            <td> 38F </td>
-            <td> 37F </td>
-          </tr>
-        </table>
+        <div class = "App-weekly-view">
+          <h4> Weekly Weather Forecast </h4>
+          <table class = "App-table">
+            <tr>
+              <th> Monday </th>
+              <th> Tuesday </th>
+              <th> Wednesday </th>
+              <th> Thursday </th>
+              <th> Friday </th>
+              <th> Saturday </th>
+              <th> Sunday </th>
+            </tr>
+            <tr>
+              <td> 52F </td>
+              <td> 48F </td>
+              <td> 50F </td>
+              <td> 58F </td>
+              <td> 52F </td>
+              <td> 53F </td>
+              <td> 45F </td>
+            </tr>
+            <tr>
+              <td> 30F </td>
+              <td> 37F </td>
+              <td> 32F </td>
+              <td> 40F </td>
+              <td> 45F </td>
+              <td> 38F </td>
+              <td> 37F </td>
+            </tr>
+          </table>
+        </div>
         </div>
       </body>
     </div>
